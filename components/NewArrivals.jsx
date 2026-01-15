61aeec4fcc6492ebe8c4109a37ca0fc660ddeb5a
Mark Kapileo <mckrune@gmail.com>
2026-01-15 15:17:06 -0500

    animation

--- components/NewArrivals.jsx
+++ components/NewArrivals.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Title from './Title'
 import { dummyProducts } from '@/public/data'
 import { Item } from './Item'
 
 
 function NewArrivals() {
+  const [stopScroll, setStopScroll] = useState(false)
+
   return (
+    <>
+    <style>
+        {`
+            .marquee-inner {
+                animation: marqueeScroll linear infinite;
+            }
+            @keyframes marqueeScroll {
+                0%{
+                    transform: translateX(0%)
+                }
+                100%{
+                    transform: translateX(-50%)
+                }
+            }
+        `}
+    </style>
     <section className='max-padd-container py-16 xl:py-28'>
         <Title title1={"New"} title2={"Arrivals"} titleStyles={"pb-10"} />
-        <div>
-            <div/>
-            <div>
+        <div className='overflow-hidden relative' onMouseEnter={()=> setStopScroll(true)} onMouseLeave={()=> setStopScroll(false)}>
+            <div className='absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-linear-to-r from-background to-transparent'/>
+            <div className='marquee-inner flex w-fit' style={{animationPlayState: stopScroll ? "paused" : "running", animationDuration: 22000 + "ms"}}>
                 <div className='flex'>
                     {dummyProducts.slice(0, 8).map((product, index)=>(
                         <div key={index} className='w-56 mx-5 h-103 relative'>
@@ -19,8 +38,10 @@ function NewArrivals() {
                     ))}
                 </div>
             </div>
+            <div className='absolute right-0 top-0 h-full w-20 z-10 pointer-events-none bg-linear-to-l from-background to-transparent'/>
         </div>
     </section>
+    </>
   )
 }
 
