fb737d3aad8f148f960b91ed03bbc77c49b0258f
Mark Kapileo <mckrune@gmail.com>
2026-01-21 15:52:23 -0500

    Cart Function

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -12,11 +12,56 @@ export const useAppContext = ()=> {
 function AppContextProvider (prop) {
     const [products, setProducts] = useState([])
     const [isSeller, setIsSeller] = useState(false)
+    const [cartItems, setCartItems] = useState({})
+    const [user, setUser] = useState(true)
     const router = useRouter()
     const currency = process.env.NEXT_PUBLIC_CURRENCY || "$"
 
     const getProducts = ()=>{
         setProducts(dummyProducts)
+    }
+
+    const addToCart = (itemId)=>{
+        let cartData = structuredClone(cartItems)
+        if(cartData[itemId]){
+            cartData[itemId] += 1
+        }else {
+            cartData[itemId] = 1
+        }
+
+        setCartItems(cartData)
+        console.log(cartItems)
+    }
+
+    const getCartCount = ()=>{
+        let totalCount = 0
+        for(const itemId in cartItems){
+            if(cartItems[itemId] > 0){
+                totalCount += cartItems[itemId]
+            }
+        }
+        return totalCount
+    }
+
+    const updateCartQuantity = (itemId, quantity)=>{
+        let cartData = structuredClone(cartItems)
+        if(quantity === 0){
+            delete cartData[itemId]
+        }else {
+            cartData[itemId] = quantity
+        }
+        setCartItems(cartData)
+    }
+
+    const getCartAmount = ()=>{
+        let totalAmount = 0
+        for(const itemId in cartItems){
+            let itemInfo = products.find(product => product._id === itemId)
+            if(cartItems[itemId] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[itemId]
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100
     }
 
     useEffect(()=>{
@@ -27,7 +72,15 @@ function AppContextProvider (prop) {
         products,
         isSeller,
         setIsSeller,
-        currency
+        currency,
+        router,
+        cartItems,
+        setCartItems,
+        addToCart,
+        getCartCount,
+        updateCartQuantity,
+        getCartAmount,
+        user,
 
     }
 
